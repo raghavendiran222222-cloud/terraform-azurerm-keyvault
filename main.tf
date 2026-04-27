2854f19f79ec478788611dac9f1cd5166216cce3
raghavendiran222222-cloud <raghavendiran222222@gmail.com>
2026-04-27 14:25:02 +0530

    add main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+# === Key Vault ===
+resource "azurerm_key_vault" "this" {
+  name                        = var.name
+  location                    = var.location
+  resource_group_name         = var.resource_group_name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = var.sku_name
+  soft_delete_retention_days  = var.soft_delete_retention_days
+  purge_protection_enabled    = true
+  tags                        = var.tags
+}
+
+data "azurerm_client_config" "current" {}
